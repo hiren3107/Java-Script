b37de4a6e6cc7a9763b353976d98bff3447c2f80
hiren3107 <agravathiren07@gmail.com>
2024-07-18 10:51:58 +0530

    Day 2

---
+++ Day 2/script.js
@@ -0,0 +1,74 @@
+// Q1
+
+let age = 25;
+
+document.getElementById("h").innerHTML = `<h1>Q1</h1> <h1>${age}</h1>`
+
+// Q2
+
+
+let name = "Alice"
+
+name = "Bob"
+
+document.getElementById("i").innerHTML = `<h1>Q2</h1> <h1>${name}</h1>`
+
+// Q3
+
+// const pi = 3.14;
+// pi = 3.1415;
+
+// document.getElementById("r").innerHTML = `<h1>Q2</h1> <h1>${pi}</h1>`
+
+// Q4
+
+var x, y, z;
+
+x = 10;
+y = 20;
+z = x + y;
+
+document.getElementById("e").innerHTML = `<h1>Q4</h1> <h1>x =${x}</h1><h1>y =${y}</h1><h1>z =${z}</h1>`
+
+// Q6
+
+var k;
+
+document.getElementById("n").innerHTML = `<h1>Q6</h1> <h1>${k}</h1>`
+
+console.log(k);
+
+// Q7
+
+let j = null;
+
+document.getElementById("a").innerHTML = `<h1>Q7</h1> <h1>${j}</h1>`
+
+console.log(j);
+
+// Q8
+
+var Alice1 = "Hello, Alice!";
+
+document.getElementById("g").innerHTML = `<h1>Q8</h1> <h1>${Alice1}</h1>`
+
+
+// Q9  find the area of a circle
+//  πr*r
+
+const p = 3.14;
+var ans, r = 7;
+
+ans = p * r * r;
+
+document.getElementById("v").innerHTML = `<h1>Q9</h1> <h1>${ans}</h1>`
+
+
+// Q10 Q.10 find the area of a rectangle.
+
+var rectangle , l = 10 , w = 15 ;
+
+rectangle = l * w ;
+
+document.getElementById("t").innerHTML = `<h1>Q10</h1> <h1>${rectangle}</h1>`
+
